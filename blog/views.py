a905c9c7f0bb9187ae6fee837de75aebc8040cca
brin2806 <brindha@example.com>
2015-07-25 19:38:49 +1000

    modified templates to dispaly posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+'''Dot after from means current directory or current application. Since views.py and models.py are in the same directory we can simply use . and the 
+name of the file (without .py). Then we import the name of the model (Post).'''
 
 # Create your views here.
 
@@ -9,4 +13,6 @@ will render (put together) our
 template blog/post_list.html. '''
 
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'blog/post_list.html', {'posts': posts})
+
