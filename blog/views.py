6798c9eb6b99f3aea425d2cd5ea3b68a27a1f6bb
brin2806 <brindha@example.com>
2015-07-25 11:59:44 +1000

    changed the HTML for the site

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+''' As you can see, we created a method (def) 
+called post_list that takes request 
+and return a method render that 
+will render (put together) our 
+template blog/post_list.html. '''
+
+def post_list(request):
+	return render(request, 'blog/post_list.html', {})
