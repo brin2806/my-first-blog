68450df3bbb014bfe171c4314a49a7254cae959f
brin2806 <brindha@example.com>
2015-07-26 17:48:37 +1000

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+
 from .models import Post
 '''Dot after from means current directory or current application. Since views.py and models.py are in the same directory we can simply use . and the 
 name of the file (without .py). Then we import the name of the model (Post).'''
@@ -16,3 +18,6 @@ def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts': posts})
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
